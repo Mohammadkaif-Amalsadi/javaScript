c63530fea65192a7ca79418f5d7e9ef2b01096ec
Mohammadkaif Iqbal Amalsadi <amalsadikaif825@gmail.com>
2025-11-10 10:59:30 +0530

    Added a new feature and fixed the login bug

---
+++ first.js
@@ -0,0 +1,9 @@
+console.log("This is the first JavaScript file.");
+function greet(name) {
+  return `Hello, ${name}!`;
+}
+console.log(greet("World"));
+function add(a, b) {
+  return a + b;
+}
+console.log(`2 + 3 = ${add(2, 3)}`);
